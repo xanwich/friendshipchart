08a19cc0bfc5530a631eb3622606a97f1ca53f3b
Xander Beberman <xander@xanderb.com>
2024-03-14 13:28:24 -0400

    add friendship search

--- graph.py
+++ graph.py
@@ -11,20 +11,23 @@ matplotlib.use("agg")
 
 
 class Grapher:
-    def __init__(self, edges):
+    def __init__(self, edges, color=(1, 1, 1, 0.8)):
         self.G = nx.Graph()
         self.G.add_edges_from([(e.l, e.r) for e in edges])
+        self.color = color
 
-    def make_image(self):
+    def make_image(self, edge_colors=None, node_colors=None):
         plt.clf()
         plt.figure(figsize=(12, 12))
-        pos = nx.spring_layout(self.G, scale=20, k=2 / (self.G.order() ** 0.5))
+        pos = nx.spring_layout(
+            self.G, scale=20, k=2 / (self.G.order() ** 0.9), iterations=100
+        )
         nx.draw_networkx(
             self.G,
             pos=pos,
-            node_color=(1, 1, 1, 0.8),
             node_size=150,
-            edge_color=(1, 1, 1, 0.8),
+            node_color=node_colors if node_colors else self.color,
+            edge_color=edge_colors if edge_colors else self.color,
             font_family="serif",
         )
         plt.axis("off")
@@ -32,3 +35,39 @@ class Grapher:
         plt.savefig(img, format="svg", bbox_inches="tight", transparent=True)
         img.seek(0)
         return base64.b64encode(img.getvalue()).decode("utf-8")
+
+    def shortest_path(self, l, r):
+        path = nx.shortest_path(self.G, l, r)
+        return path
+
+    def shortest_path_text(self, path):
+        if len(path) > 1:
+            return (
+                path[0]
+                + " knows "
+                + " who knows ".join(path[1:])
+                + f" ({len(path) - 1}°)"
+            )
+        elif len(path) == 1:
+            return path[0] + " knows themself!"
+        else:
+            return "sorry"
+
+    def make_shortest_path_image(self, path, color=(1, 0.5, 0.1, 0.8)):
+        l = path[0]
+        r = path[-1]
+        path_edges = list(zip(path, path[1:]))
+        # Create a list of all edges, and assign colors based on whether they are in the shortest path or not
+        edge_colors = [
+            (
+                color
+                if edge in path_edges or tuple(reversed(edge)) in path_edges
+                else self.color
+            )
+            for edge in self.G.edges()
+        ]
+        node_colors = [
+            ("yellow" if n == l or n == r else color if n in path else self.color)
+            for n in self.G.nodes
+        ]
+        return self.make_image(edge_colors=edge_colors, node_colors=node_colors)
--- routes.py
+++ routes.py
@@ -3,6 +3,11 @@ from app import app, db
 from models import Edge
 from forms import EdgeForm
 from graph import Grapher
+from networkx import NetworkXNoPath
+
+
+def people_from_edges(edges):
+    return sorted({e.l for e in edges}.union({e.r for e in edges}))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -19,12 +24,35 @@ def index():
             db.session.add(edge)
             db.session.commit()
     edges = db.session.query(Edge).order_by(Edge.id.desc()).all()
-    people = sorted({e.l for e in edges}.union({e.r for e in edges}))
+    people = people_from_edges(edges)
     return render_template("index.html", form=form, edges=edges, people=people)
 
 
-@app.route("/graph")
+@app.route("/graph", methods=["GET"])
 def graph():
     edges = db.session.query(Edge).all()
+    people = people_from_edges(edges)
+    form = EdgeForm()
     g = Grapher(edges)
-    return render_template("graph.html", graph=g.make_image())
+    l = request.args.get("l")
+    r = request.args.get("r")
+    text = None
+    if l and l not in people:
+        text = f"{l} isn't friends with anyone yet!"
+        graph = g.make_image()
+    elif r and r not in people:
+        text = f"{r} isn't friends with anyone yet!"
+        graph = g.make_image()
+    elif l and r:
+        try:
+            path = g.shortest_path(l, r)
+            graph = g.make_shortest_path_image(path)
+            text = g.shortest_path_text(path)
+        except NetworkXNoPath:
+            graph = g.make_image()
+            text = f"No friendship found between {l} and {r}! Maybe you should introduce them"
+    else:
+        graph = g.make_image()
+    return render_template(
+        "graph.html", graph=graph, text=text, form=form, people=people
+    )
